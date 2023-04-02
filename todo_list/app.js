675279bd4d584a57b4a03ef427905233f4521c71
ellen <ellentyyu@gmail.com>
2023-04-02 14:33:13 +0800

    feat: todo list project init/ 連線資料庫

---
+++ todo_list/app.js
@@ -0,0 +1,33 @@
+// 載入 Express.js
+const express = require('express');
+// 載入 mongoose
+const mongoose = require('mongoose');
+
+// 僅在非正式環境時 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express();
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); // 設定連線到 DB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+// 設定首頁路由
+app.get('/', (req, res) => {
+  res.send('hello world!!!!');
+});
+
+// 設定 port 3000
+app.listen(3000, () => {
+  console.log('app running on http://localhost:3000');
+});
