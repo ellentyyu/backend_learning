a724f2f76ab9da17322abc1b7e70d78769d9e1a2
ellen <ellentyyu@gmail.com>
2023-04-08 14:48:45 +0800

    feat: add handlebars

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,14 +1,16 @@
 // 載入 Express.js
 const express = require('express');
+// 載入樣板引擎
+const exphbs = require('express-handlebars');
 // 載入 mongoose
 const mongoose = require('mongoose');
-
 // 僅在非正式環境時 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 const app = express();
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); // 設定連線到 DB
 
 // 取得資料庫連線狀態
@@ -22,9 +24,13 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// 樣板引擎
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world!!!!');
+  res.render('index');
 });
 
 // 設定 port 3000
