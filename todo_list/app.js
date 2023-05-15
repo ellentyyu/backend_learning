fd550e1ef131adb76bb619a6051fe65919c3bbfb
ellen <ellentyyu@gmail.com>
2023-05-15 22:43:10 +0800

    refactor: 抽出mongoose連線設定

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,26 +1,12 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-const Todo = require('./models/todo');
-const mongoose = require('mongoose');
+
 const methodOverride = require('method-override');
 const routes = require('./routes');
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require('./config/mongoose');
 
 const app = express();
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
--- todo_list/models/seeds/todoSeeder.js
+++ todo_list/models/seeds/todoSeeder.js
@@ -1,18 +1,5 @@
-// 載入 mongoose 和 todo model
-const mongoose = require('mongoose');
 const Todo = require('../todo');
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); // 連線到 DB
-
-// 連線狀態
-const db = mongoose.connection;
-db.on('error', () => {
-  console.log('mongodb error!');
-});
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
   console.log('mongodb connected!');
