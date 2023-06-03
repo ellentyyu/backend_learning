14392f2b17b5b33a723e27632283d4bee5f054e2
ellen <ellentyyu@gmail.com>
2023-06-03 15:06:46 +0800

    feat: 加入user路由模組和login頁面

--- todo_list/routes/index.js
+++ todo_list/routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const home = require('./modules/home');
 const todos = require('./modules/todos');
+const user = require('./modules/user');
 
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home);
 router.use('/todos', todos);
+router.use('/user', user);
 
 module.exports = router;
