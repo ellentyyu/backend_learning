d50133ff7ee600e89aa2cb5a5f883db5728ca458
ellen <ellentyyu@gmail.com>
2023-06-03 16:36:39 +0800

    feat: 重新命名users路由/新增註冊路由和頁面

--- todo_list/routes/index.js
+++ todo_list/routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const home = require('./modules/home');
 const todos = require('./modules/todos');
-const user = require('./modules/user');
+const users = require('./modules/users');
 
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home);
 router.use('/todos', todos);
-router.use('/user', user);
+router.use('/users', users);
 
 module.exports = router;
