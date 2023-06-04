783740f621460c9519bacb14e3860b6a3a1fcf6b
ellen <ellentyyu@gmail.com>
2023-06-04 15:42:10 +0800

    feat: 加入登入功能

--- todo_list/routes/modules/users.js
+++ todo_list/routes/modules/users.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../../models/user');
+const passport = require('passport');
 
 router.get('/login', (req, res) => {
-  res.render('login');
+    res.render('login');
 });
-router.post('/login', (req, res) => {
-});
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+}));
+
 router.get('/register', (req, res) => {
-  res.render('register')
+    res.render('register')
 });
 
 router.post('/register', (req, res) => {
