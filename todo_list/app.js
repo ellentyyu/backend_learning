a2dde537c3281ec98e6b923da4ba29d826d22781
ellen <ellentyyu@gmail.com>
2023-06-04 14:58:02 +0800

    feat: 加入session設定

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-
 const methodOverride = require('method-override');
+
 const routes = require('./routes');
 require('./config/mongoose');
 
@@ -10,6 +11,12 @@ const app = express();
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
+
+app.use(session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
